bc5efd9408b1f51b81151ea085bf1ce1d51e3a87
readock <readock@gmx.de>
2018-02-27 18:32:50 +0100

    Implements basic functionality

---
+++ ReferencePresenter/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ReferencePresenter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(535, 374);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(20, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(507, 85);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "open image: drag and drop\r\ncontext menue: right mouse\r\nmove: left or middle mouse" +
+    " button\r\nresize: bottom right corner\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Font = new System.Drawing.Font("Elephant", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(515, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Reference Presenter";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(539, 378);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(20, 39);
+            this.Name = "Form1";
+            this.Text = "ReferencePresenter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReferencePresenter/Form1.cs
@@ -0,0 +1,300 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReferencePresenter {
+    public partial class Form1 : Form {
+
+        private Image CurrentImage;
+        private Bitmap CurrentImageGrayscale;
+
+        private const int cGrip = 20;      // Grip size
+        private const int cCaption = 15;   // Caption bar height;
+
+        private bool mouseDown;
+        private Point lastLocation;
+        private bool borderVisible = true;
+        private bool grayscale = false;
+        private bool alwaysOnTop = true;
+        private bool autoResize = true;
+        MenuItem menuItemGrayscale = new MenuItem("grayscale");
+
+        private const int MINMAX = 100;
+        private const double ZOOMFACTOR = 1.2;
+
+        public Form1(string picture) {
+            InitializeComponent();
+            InitEventListener();
+            InitContextMenu();
+            InitFormWindow();
+            InitPictureBox(picture);
+        }
+
+        private void InitPictureBox(string picture) {
+            if (picture != null) {
+                label1.Visible = false;
+                label2.Visible = false;
+                LoadImage(picture);
+            }
+        }
+
+        private void InitFormWindow() {
+            Toggleborder();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.TopMost = alwaysOnTop;
+        }
+
+        public void LoadImage(string file) {
+            if (label1.Visible) {
+                label1.Visible = false;
+                label2.Visible = false;
+            }
+            try {
+                CurrentImage = Image.FromFile(file);
+            } catch (Exception e) {
+                MessageBox.Show("Could not load image!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            pictureBox1.Image = CurrentImage;
+            menuItemGrayscale.Checked = false;
+            CurrentImageGrayscale = new Bitmap((Image)CurrentImage.Clone());
+            if (autoResize)
+                ResizeForm();
+            new Thread(() => {
+                CurrentImageGrayscale = ConvertToGrayScale(CurrentImageGrayscale);
+                if (grayscale)
+                    pictureBox1.Invoke(new Action(() => { pictureBox1.Image = CurrentImageGrayscale; }));
+            }).Start();
+        }
+
+        private void InitEventListener() {
+            label1.MouseDown += PictureBox1_MouseDown;
+            label1.MouseUp += PictureBox1_MouseUp;
+            label1.MouseMove += PictureBox1_MouseMove;
+            label2.MouseDown += PictureBox1_MouseDown;
+            label2.MouseUp += PictureBox1_MouseUp;
+            label2.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        private void InitContextMenu() {
+            ContextMenu cm = new ContextMenu();
+
+            AddChangeImageOption(cm);
+            AddResizeOption(cm);
+            cm.MenuItems.Add("-");
+
+            AddGrayScaleOption(cm);
+            AddFlippOption(cm);
+            cm.MenuItems.Add("-");
+
+            AddToggleWindowBorderOption(cm);
+            AddAutoResizeOption(cm);
+            AddToggleAlwaysOnTopOption(cm);
+            AddChangeBackgroundColor(cm);
+            cm.MenuItems.Add("-");
+
+            cm.MenuItems.Add("close", (a, b) => Environment.Exit(0));
+
+            pictureBox1.ContextMenu = cm;
+        }
+
+        #region ContextMenue Options
+        private void AddChangeBackgroundColor(ContextMenu cm) {
+            var itemColor = new MenuItem("change color");
+            itemColor.Click += (a, b) => {
+                var dialog = new ColorDialog();
+                var result = dialog.ShowDialog();
+                if (result == DialogResult.OK) {
+                    label1.BackColor = dialog.Color;
+                    label2.BackColor = dialog.Color;
+                    pictureBox1.BackColor = dialog.Color;
+                }
+            };
+            cm.MenuItems.Add(itemColor);
+        }
+
+        private void AddToggleAlwaysOnTopOption(ContextMenu cm) {
+            var itemAlwaysTop = new MenuItem("always on top") { Checked = alwaysOnTop };
+            itemAlwaysTop.Click += (a, b) => {
+                alwaysOnTop = !alwaysOnTop;
+                itemAlwaysTop.Checked = alwaysOnTop;
+                this.TopMost = alwaysOnTop;
+            };
+            cm.MenuItems.Add(itemAlwaysTop);
+        }
+
+        private void AddAutoResizeOption(ContextMenu cm) {
+            var itemAutoResize = new MenuItem("auto resize") { Checked = autoResize };
+            itemAutoResize.Click += (a, b) => {
+                autoResize = !autoResize;
+                itemAutoResize.Checked = autoResize;
+                ResizeForm();
+            };
+            cm.MenuItems.Add(itemAutoResize);
+        }
+
+        private void AddGrayScaleOption(ContextMenu cm) {
+            menuItemGrayscale.Click += (a, b) => {
+                if (grayscale)
+                    pictureBox1.Image = CurrentImage;
+                else
+                    pictureBox1.Image = CurrentImageGrayscale;
+                grayscale = !grayscale;
+                menuItemGrayscale.Checked = grayscale;
+            };
+            cm.MenuItems.Add(menuItemGrayscale);
+        }
+        private void AddFlippOption(ContextMenu cm) {
+            var flipp = new MenuItem("flipp horizontal");
+            flipp.Click += (a, b) => {
+                CurrentImageGrayscale?.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                CurrentImage?.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Image = pictureBox1.Image;
+            };
+            cm.MenuItems.Add(flipp);
+        }
+
+        private void AddToggleWindowBorderOption(ContextMenu cm) {
+            var itemBorder = new MenuItem("window border");
+            itemBorder.Click += (a, b) => {
+                Toggleborder();
+                itemBorder.Checked = borderVisible;
+            };
+            cm.MenuItems.Add(itemBorder);
+        }
+
+        private void AddResizeOption(ContextMenu cm) {
+            var itemResize = new MenuItem("resize");
+            itemResize.Click += (a, b) => {
+                ResizeForm();
+            };
+            cm.MenuItems.Add(itemResize);
+        }
+
+        private void AddChangeImageOption(ContextMenu cm) {
+            var itemChangeImage = new MenuItem("change image");
+            itemChangeImage.Click += (a, b) => {
+                var dialog = new OpenFileDialog() {
+                    Title = "Select a image",
+                };
+                if (dialog.ShowDialog() == DialogResult.OK) {
+                    LoadImage(dialog.FileName);
+                }
+            };
+            cm.MenuItems.Add(itemChangeImage);
+        }
+        #endregion
+
+
+        private void ResizeForm() {
+            if (CurrentImage == null)
+                return;
+            var centerPoint = new Point(this.Location.X + this.Width / 2, this.Location.Y + this.Height / 2);
+            if (pictureBox1.Image.Width / (double)this.Width > pictureBox1.Image.Height / (double)this.Height) {
+                double scale = pictureBox1.Image.Width / (double)this.Width;
+                this.Height = (int)Math.Round(pictureBox1.Image.Height / scale);
+            } else {
+                double scale = pictureBox1.Image.Height / (double)this.Height;
+                this.Width = (int)Math.Round(pictureBox1.Image.Width / scale);
+            }
+            this.SetDesktopLocation((int)Math.Round(centerPoint.X - this.Width / 2.0), (int)Math.Round(centerPoint.Y + 0.0 - this.Height / 2.0));
+        }
+
+        private void Toggleborder() {
+            if (borderVisible) {
+                this.TransparencyKey = Color.Turquoise;
+                this.BackColor = Color.Turquoise;
+            } else {
+                this.TransparencyKey = Color.Turquoise;
+                this.BackColor = Color.Green;
+            }
+            borderVisible = !borderVisible;
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            if (mouseDown) {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e) {
+            mouseDown = false;
+        }
+        protected override void OnPaint(PaintEventArgs e) {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+        }
+
+        protected override void WndProc(ref Message m) {
+            if (m.Msg == 0x84) {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption) {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    if (autoResize)
+                        ResizeForm();
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip) {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    if (autoResize)
+                        ResizeForm();
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Count() > 1) {
+                foreach (string file in files)
+                    Console.WriteLine(file);
+            }
+            LoadImage(files.First());
+        }
+
+        public Bitmap ConvertToGrayScale(Bitmap Bmp) {
+            int rgb;
+            Color c;
+
+            for (int y = 0; y < Bmp.Height; y++)
+                for (int x = 0; x < Bmp.Width; x++) {
+                    c = Bmp.GetPixel(x, y);
+                    rgb = (int)((c.R + c.G + c.B) / 3);
+                    Bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            return Bmp;
+        }
+    }
+}
